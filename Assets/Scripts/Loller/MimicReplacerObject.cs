8cd1c1ff92e11f983d8e846d2e857d7221d6982f
Handensaken <maxmax.larsson@gmail.com>
2024-09-15 11:41:57 +0200

    Updated mimics

---
+++ Assets/Scripts/Loller/MimicReplacerObject.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class MimicReplacerObject : MonoBehaviour
+{
+    public GameObject replacer;
+    private List<GameObject> gameObjects = new List<GameObject>();
+
+    void OnGUI()
+    {
+        gameObjects = GameObject.FindGameObjectsWithTag("Mimicable").ToList();
+           foreach (GameObject G in gameObjects){
+                Vector3 pos = G.transform.position;
+                Quaternion rot = G.transform.rotation;
+                Instantiate(replacer, pos, rot);
+                DestroyImmediate(G);
+                Debug.Log(gameObjects.Count);
+            }
+    }
+
+    // Update is called once per frame
+    void Update() { 
+        if(Input.GetKeyDown(KeyCode.G)){
+            Debug.Log("Fucktou");
+        }
+    }
+    void Execute(){
+
+    }
+}
